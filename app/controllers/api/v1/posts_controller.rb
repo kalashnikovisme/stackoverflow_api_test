dfa3719e0853e9ab48c2b262987ab4b218ef1ef8
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-12-20 00:09:22 +0400

    Some refactoring

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::PostsController < ApplicationController
   def index
     # I should use here delayed job, but creating websocket app is not required condition of task
-    results = JSON.parse ::Stackoverflow::SearchApiWrapper.search params[:query]
-    render json: results["items"].map { |h| h.slice('title', 'link') }
+    results = ::Stackoverflow::SearchApiWrapper.search params[:query]
+    render json: results
   end
 end
--- lib/stackoverflow/search_api_wrapper.rb
+++ lib/stackoverflow/search_api_wrapper.rb
@@ -3,6 +3,12 @@ require 'net/http'
 module Stackoverflow::SearchApiWrapper
   class << self
     def search(query)
+      JSON.parse(request(query))["items"].map { |h| h.slice('title', 'link') }
+    end
+
+    private
+
+    def request(query)
       url = URI.parse("https://api.stackexchange.com/2.2/search/advanced?order=desc&sort=activity&q=#{query}&site=stackoverflow")
       req = Net::HTTP::Get.new(url.request_uri, 'Content-Type' => 'application/json')
       http = Net::HTTP.new(url.host, url.port)
