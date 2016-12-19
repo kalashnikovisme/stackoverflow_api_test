90b6d818551c3e2049bfea2556adabdac4a0668e
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-12-20 00:02:02 +0400

    Add comment

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::PostsController < ApplicationController
   def index
+    # I should use here delayed job, but creating websocket app is not required condition of task
     results = JSON.parse ::Stackoverflow::SearchApiWrapper.search params[:query]
     render json: results["items"].map { |h| h.slice('title', 'link') }
   end
